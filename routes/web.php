1ea53256802c59bb7aa2310616f583d157324221
Hanifah Amany <2241720127@student.polinema.ac.id>
2024-03-07 20:46:20 +0700

    PR articles

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,10 @@ Route::get('/user/{name}', function ($name) {
     return 'Nama saya '.$name;
 });
     
-Route::get('/posts/{post}/comments/{comment}', function 
-($postId, $commentId) {
- return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman artikel dengan ID '.$id;
 });
